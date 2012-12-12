6a4b958ad9d626710b0c7feba97b044dca9ccf6a
Jonathan Alviar <jonalviar@gmail.com>
2012-12-12 01:17:57 -0800

    update app.js

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ app.get('/', function(req, res){
 });
 
 
-app.listen(3000);
-console.log('Listening on port 3000');
-//console.log(app.routes)
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
 
